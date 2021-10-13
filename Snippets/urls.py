5da4f53c9e792bd4fc913ad67fc1c7d03607d1e4
Aleksandr Petrov <aleksandr.petrov@x5.ru>
2021-10-13 20:32:55 +0300

    add commit for snippets

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
-]
+    path('', views.index_page, name='homepage'),
+    path('snippets/add', views.add_snippet_page, name='snippets-add'),
+    path('snippets/list', views.snippets_page, name='snippets-list'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
